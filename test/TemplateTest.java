953d12a51ad2631a34e6ec6307040edbaecdaa65
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-11-26 12:42:50 +0900

    add template model

---
+++ test/TemplateTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+
+import java.util.List;
+import play.test.*;
+import models.*;
+
+public class TemplateTest extends UnitTest {
+
+    @Test
+    public void create() {
+    	User user = new User().save();
+    	String name = "My Template Name";
+    	String subject = "My Mail Subject";
+    	Template template = new Template(name, subject, "body", user);
+    	template.save();
+    	
+    	Template findTemplte = Template.findById(template);
+    	
+    	assertEquals(findTemplte.name, name);
+    }
+
+}
