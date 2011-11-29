23c45df48f685574deb3cd1bc3625886e20d3a7a
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-11-29 15:30:46 +0900

    formats

--- app/models/Facebook.java
+++ app/models/Facebook.java
@@ -8,40 +8,40 @@ import play.mvc.results.Redirect;
 
 public class Facebook extends OAuth2 {
 
-	public final static String AUTH_URL = "https://graph.facebook.com/oauth/authorize";
-	public final static String ACCESS_TOKEN_URL = "https://graph.facebook.com/oauth/access_token";
-	public final static String CLIENT_ID = "";
-	public final static String SECRET = "";
+    public final static String AUTH_URL = "https://graph.facebook.com/oauth/authorize";
+    public final static String ACCESS_TOKEN_URL = "https://graph.facebook.com/oauth/access_token";
+    public final static String CLIENT_ID = "";
+    public final static String SECRET = "";
 
-	public Facebook() {
-		super(AUTH_URL, ACCESS_TOKEN_URL, CLIENT_ID, SECRET);
-	}
+    public Facebook() {
+        super(AUTH_URL, ACCESS_TOKEN_URL, CLIENT_ID, SECRET);
+    }
 
-	/**
-	 * redirects the user to the authorization page with scope.
-	 * @param callbackURL
-	 * @param scope
-	 */
-	public void retrieveVerificationCode(String callbackURL, String scope) {
-		throw new Redirect(authorizationURL
-				+ "?client_id=" + clientid
-				+ "&redirect_uri=" + callbackURL
-				+ "&scope=" + scope);
-	}
-	
-	public String getAuthorizationURL() {
-		return authorizationURL;
-	}
-	
-	public void setAuthorizationURL(String _autorizationURL) {
-		authorizationURL = _autorizationURL;
-	}
+    /**
+     * redirects the user to the authorization page with scope.
+     * @param callbackURL
+     * @param scope
+     */
+    public void retrieveVerificationCode(String callbackURL, String scope) {
+        throw new Redirect(authorizationURL
+                + "?client_id=" + clientid
+                + "&redirect_uri=" + callbackURL
+                + "&scope=" + scope);
+    }
 
-	public String getClientid() {
-		return clientid;
-	}
-	
-	public void setClientid(String _clientid) {
-		clientid = _clientid;
-	}	
+    public String getAuthorizationURL() {
+        return authorizationURL;
+    }
+
+    public void setAuthorizationURL(String _autorizationURL) {
+        authorizationURL = _autorizationURL;
+    }
+
+    public String getClientid() {
+        return clientid;
+    }
+
+    public void setClientid(String _clientid) {
+        clientid = _clientid;
+    }
 }
